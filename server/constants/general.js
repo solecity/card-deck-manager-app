ac20fd6fadbf75f33296bfcfde056a4730c72b33
Mariana Moreira <marianaccmoreira@gmail.com>
2021-05-19 00:36:07 +0100

    add server constants

---
+++ server/constants/general.js
@@ -0,0 +1,6 @@
+export const USER_TYPES = {
+  ADMIN: 1,
+  STANDARD: 2
+};
+
+export const DATE_FORMAT = "yyyy-MM-dd";
---
+++ server/constants/messages.js
@@ -0,0 +1,27 @@
+export const LOGIN = {
+  UNAUTHORIZED: "Wrong username or password"
+};
+
+export const USER = {
+  CREATED: "User created successfully",
+  DELETED: "User deleted successfully",
+  UPDATED: "User updated successfully",
+  NOT_FOUND: "User not found",
+  INVALID_ID: "User id not valid"
+};
+
+export const COLLECTION = {
+  CREATED: "Collection created successfully",
+  DELETED: "Collection deleted successfully",
+  UPDATED: "Collection updated successfully",
+  NOT_FOUND: "Collection not found",
+  INVALID_ID: "Collection id not valid"
+};
+
+export const CARD = {
+  CREATED: "Card created successfully",
+  DELETED: "Card deleted successfully",
+  UPDATED: "Card updated successfully",
+  NOT_FOUND: "Card not found",
+  INVALID_ID: "Card id not valid"
+};
