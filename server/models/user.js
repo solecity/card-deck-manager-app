2d94ea9c8d0c1e989efdb24986988f2784583e45
Mariana Moreira <marianaccmoreira@gmail.com>
2021-05-19 00:34:50 +0100

    add user model

---
+++ server/models/user.js
@@ -0,0 +1,54 @@
+// libraries
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+// utils
+import { formatDate } from "../utils/date.js";
+
+const { Schema } = mongoose;
+const saltRounds = 10;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false
+  },
+  type: {
+    type: String,
+    default: 2
+  },
+  createdAt: {
+    type: String,
+    default: formatDate(new Date())
+  }
+});
+
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    bcrypt.hash(this.password, saltRounds, (err, hash) => {
+      if (err) {
+        next(err);
+      } else {
+        this.password = hash;
+        next();
+      }
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
