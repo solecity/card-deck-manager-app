94f88a881807f52eaf229cc026357c7405d0c327
Mariana Moreira <marianaccmoreira@gmail.com>
2021-05-19 00:35:56 +0100

    initial server setup

---
+++ server/index.js
@@ -0,0 +1,56 @@
+// libraries
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// routes
+import routes from "./routes/index.js";
+
+// config
+import { URI } from "./config/database.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/login", routes.auth);
+app.use("/users", routes.user);
+
+const PORT = process.env.PORT || 5000;
+
+// connect database
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error));
+
+if (process.env.NODE_ENV === "dev") {
+  mongoose.set("debug", true);
+}
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+// build
+if (process.env.NODE_ENV === "prod") {
+  app.use(express.static("../client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
