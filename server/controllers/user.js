8c83fce0c2298747219f870e3800e701533d53c2
Mariana Moreira <marianaccmoreira@gmail.com>
2021-05-19 00:36:30 +0100

    add user controllers

---
+++ server/controllers/user.js
@@ -0,0 +1,72 @@
+// libraries
+import httpStatus from "http-status-codes";
+
+// model
+import User from "../models/user.js";
+
+// constants
+import { USER } from "../constants/messages.js";
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    return res.status(httpStatus.OK).json(users);
+  } catch (error) {
+    res.status(httpStatus.BAD_REQUEST).json(error);
+  }
+};
+
+export const getUser = async (req, res) => {
+  try {
+    const { id: _id } = req.params;
+
+    const user = await User.findById(_id);
+
+    if (!user) {
+      res.status(httpStatus.NOT_FOUND).json({ message: USER.NOT_FOUND });
+    }
+
+    return res.status(httpStatus.OK).json(user);
+  } catch (error) {
+    res.status(httpStatus.BAD_REQUEST).json(error);
+  }
+};
+
+export const createUser = async (req, res) => {
+  try {
+    const user = new User(req.body);
+
+    await user.save();
+
+    return res.status(httpStatus.CREATED).json({ user, message: USER.CREATED });
+  } catch (error) {
+    res.status(httpStatus.BAD_REQUEST).json(error);
+  }
+};
+
+export const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // update or err
+  } catch (error) {
+    res.status(httpStatus.BAD_REQUEST).json(error);
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await User.findByIdAndDelete(id, (err, doc) => {
+      if (err || !doc) {
+        res.status(httpStatus.NOT_FOUND).json({ message: USER.NOT_FOUND });
+      }
+    });
+
+    return res.status(httpStatus.OK).json({ message: USER.DELETED });
+  } catch (error) {
+    res.status(httpStatus.BAD_REQUEST).json(error);
+  }
+};
