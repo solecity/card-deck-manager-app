1f9a9dfe2d62cfc7c469a2da2d0ee6efca218ff8
Mariana Moreira <marianaccmoreira@gmail.com>
2021-05-19 14:28:16 +0100

    add create and delete collection

--- server/controllers/collection.js
+++ server/controllers/collection.js
@@ -24,10 +24,44 @@ export const getCollection = async (req, res) => {
     const collection = await Collection.findById(_id);
 
     if (!collection) {
-      res.status(httpStatus.NOT_FOUND).json({ message: COLLECTION.NOT_FOUND });
+      return res
+        .status(httpStatus.NOT_FOUND)
+        .json({ message: COLLECTION.NOT_FOUND });
     }
 
     return res.status(httpStatus.OK).json(collection);
+  } catch (error) {
+    return res.status(httpStatus.BAD_REQUEST).json({ message: error.message });
+  }
+};
+
+export const createCollection = async (req, res) => {
+  try {
+    const collection = new Collection(req.body);
+
+    await collection.save();
+
+    return res
+      .status(httpStatus.CREATED)
+      .json({ collection, message: COLLECTION.CREATED });
+  } catch (error) {
+    return res.status(httpStatus.BAD_REQUEST).json(error);
+  }
+};
+
+export const deleteCollection = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await Collection.findByIdAndDelete(id, (err, doc) => {
+      if (err || !doc) {
+        return res
+          .status(httpStatus.NOT_FOUND)
+          .json({ message: COLLECTION.NOT_FOUND });
+      }
+    });
+
+    return res.status(httpStatus.OK).json({ message: COLLECTION.DELETED });
   } catch (error) {
     return res.status(httpStatus.BAD_REQUEST).json({ message: error.message });
   }
--- server/routes/collection.js
+++ server/routes/collection.js
@@ -2,11 +2,20 @@
 import express from "express";
 
 // controller
-import { getCollections, getCollection } from "../controllers/collection.js";
+import {
+  getCollections,
+  getCollection,
+  createCollection,
+  deleteCollection
+} from "../controllers/collection.js";
 
 const router = express.Router();
 
 router.get("/", getCollections);
 router.get("/:id", getCollection);
+
+router.post("/", createCollection);
+
+router.delete("/:id", deleteCollection);
 
 export default router;
