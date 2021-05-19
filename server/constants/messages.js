df672670effd1f9fe4d62e9104a4b63a3ba8e9fe
Mariana Moreira <marianaccmoreira@gmail.com>
2021-05-19 12:20:18 +0100

    add new constants

--- server/constants/messages.js
+++ server/constants/messages.js
@@ -1,3 +1,7 @@
+export const GENERAL = {
+  INVALID_ID: "Invalid id"
+};
+
 export const LOGIN = {
   UNAUTHORIZED: "Wrong username or password"
 };
@@ -5,23 +9,19 @@ export const LOGIN = {
 export const USER = {
   CREATED: "User created successfully",
   DELETED: "User deleted successfully",
-  UPDATED: "User updated successfully",
-  NOT_FOUND: "User not found",
-  INVALID_ID: "User id not valid"
+  UPDATED: "User updated successfully"
 };
 
 export const COLLECTION = {
   CREATED: "Collection created successfully",
   DELETED: "Collection deleted successfully",
   UPDATED: "Collection updated successfully",
-  NOT_FOUND: "Collection not found",
-  INVALID_ID: "Collection id not valid"
+  NOT_FOUND: "Collection not found"
 };
 
 export const CARD = {
   CREATED: "Card created successfully",
   DELETED: "Card deleted successfully",
   UPDATED: "Card updated successfully",
-  NOT_FOUND: "Card not found",
-  INVALID_ID: "Card id not valid"
+  NOT_FOUND: "Card not found"
 };
