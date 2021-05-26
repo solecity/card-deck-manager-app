b3dd251270a216ec8ed78f1880fbf2279e505da1
Mariana Moreira <marianaccmoreira@gmail.com>
2021-05-26 09:39:47 +0100

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,34 @@
+// base
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// custom components
+import { Home, Collections, Cards, Admin } from "./pages";
+import { NavBar } from "./components";
+
+// styles
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact from="/" render={() => <Home />} />
+          <Route exact from="/login" render={() => <Home isLogin={true} />} />
+          <Route
+            exact
+            from="/register"
+            render={() => <Home isLogin={false} />}
+          />
+          <Route exact from="/collections" render={() => <Collections />} />
+          <Route exact from="/cards" render={() => <Cards />} />
+          <Route exact from="/admin" render={() => <Admin />} />
+        </Switch>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
