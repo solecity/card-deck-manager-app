4a7d818486d6ae2e86ced9ea88da0893c8485723
Mariana Moreira <marianaccmoreira@gmail.com>
2021-05-26 09:41:52 +0100

    add admin page

---
+++ client/src/pages/admin/admin.js
@@ -0,0 +1,11 @@
+// base
+import React from "react";
+
+// external components
+import { Box } from "@material-ui/core";
+
+const Admin = () => {
+  return <Box>Admin</Box>;
+};
+
+export default Admin;
---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "./home/home";
+import Collections from "./collections/collections";
+import Cards from "./cards/cards";
+import Admin from "./admin/admin";
+
+export { Home, Collections, Cards, Admin };
