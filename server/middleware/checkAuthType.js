5a9f5f5a790edbfdd45198a12505b89e98202861
Mariana Moreira <marianaccmoreira@gmail.com>
2021-05-28 00:42:32 +0100

    validate token type

---
+++ server/middleware/checkAuthType.js
@@ -0,0 +1,37 @@
+// libraries
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import httpStatus from "http-status-codes";
+
+// model
+import User from "../models/user.js";
+
+// constants
+import { LOGIN } from "../constants/messages.js";
+
+dotenv.config();
+
+export const checkAuthType = async (req, res, next) => {
+  try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (token) {
+      const userToken = jwt.verify(token, process.env.JWT_KEY);
+
+      const user = await User.findById(userToken.id);
+
+      if (!user) {
+        return res
+          .status(httpStatus.FORBIDDEN)
+          .json({ message: LOGIN.UNAUTHENTICATED });
+      }
+
+      req.user = user;
+    }
+
+    return next();
+  } catch (error) {
+    return res.status(httpStatus.BAD_REQUEST).json({ message: error.message });
+  }
+};
