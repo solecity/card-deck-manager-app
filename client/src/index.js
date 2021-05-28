514128f33b015cb995cc380667178a26c3260609
Mariana Moreira <marianaccmoreira@gmail.com>
2021-05-28 13:31:04 +0100

    add react bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,16 @@
+// base
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
+// store
 import store from "./store";
 
+// custom components
 import App from "./App";
 
+// styles
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 ReactDOM.render(
