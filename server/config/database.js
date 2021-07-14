81a032bbe2866a636a7f16ad432eafe56a1c6bb6
Mariana Moreira <marianaccmoreira@gmail.com>
2021-07-14 15:04:09 +0100

    test

--- server/config/database.js
+++ server/config/database.js
@@ -3,4 +3,4 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const URI = `mongodb+srv://test:${process.env.DB_PASS}@praticecluster.3ciuh.mongodb.net/cardsmanagerdb?retryWrites=true&w=majority`;
+export const URI = process.env.URI;
